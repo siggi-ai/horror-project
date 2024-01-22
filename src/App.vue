1679263b06784e5a61e84c229a25e9c7713f6d61
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-01-22 06:11:11 +0100

    boo!

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-slate-800 h-screen">
+      <h1 class="pt-44 pb-8 p-5 h-5 flex items-center justify-center text-7xl font-rubik text-stone-200 sm:text-9xl flex-shrink">
+        HORROR
+      </h1>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
