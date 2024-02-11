2616ff3a02d5aebabfad945eb0d26c3c90fbc9fe
Siegfried Hamm <siegfried.hamm@gmail.com>
2024-02-11 12:48:30 +0100

    .

--- src/views/Library.vue
+++ src/views/Library.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="body h-screen">
+  <div class="body h-screen bg-cover flex-shrink">
     <router-link to="/home">
       <img src="/images/arrow_left_bold_crooked.png" class="w-28"/>
     </router-link>
@@ -11,6 +11,6 @@
 
 <style scoped>
 .body {
-  background-image: url("/images/Library1.jpg");
+  background-image: url("/images/Library.jpg");
 }
 </style>
